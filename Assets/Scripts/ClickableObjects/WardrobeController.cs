6b6ac601fd93c53c524c06ee67871faccfca3af5
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-09-08 11:59:58 +0300

    Wardrobe Interactions

---
+++ Assets/Scripts/ClickableObjects/WardrobeController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WardrobeController : MonoBehaviour, IClickable
+{
+    [SerializeField]
+    GameObject leftDoor;
+
+    Vector3 openDoorRotation;
+    Vector3 closeDoorRotation;
+    bool isDoorOpen;
+
+    public WardrobeController()
+    {
+        openDoorRotation = new Vector3(-90, 0, 150);
+        closeDoorRotation = new Vector3(-90, 0, 0);
+    }
+
+    public void Click()
+    {
+        InteractionDoor();
+    }
+
+    void InteractionDoor()
+    {
+        if (!isDoorOpen)
+        {
+            leftDoor.transform.DORotate(openDoorRotation, 1f);
+            isDoorOpen = true;
+        }
+        else
+        {
+            leftDoor.transform.DORotate(closeDoorRotation, 1f).OnComplete(()=>{isDoorOpen = false;});
+        }
+
+    }
+
+
+}
